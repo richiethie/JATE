de67d060b17e3f7d6cbf21d57796c3e676633dc5
richiethie <richiethie@gmail.com>
2023-02-19 22:23:56 -0600

    workbox plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,30 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#31a9e1',
+        theme_color: '#31a9e1',
+        start_url: '/',
+        publicPath: '/',
+        icons: [{
+          src: path.resolve('src/images.logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }]
+      })
     ],
 
     module: {
